ad5478391def72e66d7f83e65251826f21418a76
DilyanK <dilyankamenov@protonmail.com>
2016-10-18 16:59:57 +0100

    Added more words to remove

--- input_parser.py
+++ input_parser.py
@@ -1,4 +1,12 @@
-words_to_remove = ["at", "please"]
+words_to_remove = ['at', 'please', 'a', 'about', 'all', 'an', 'another', 'any', 'around',
+              'bad', 'beautiful', 'been', 'better', 'big', 'can', 'every', 'for',
+              'from', 'good', 'have', 'her', 'here', 'hers', 'his', 'how',
+              'i', 'if', 'in', 'into', 'is', 'it', 'its', 'large', 'later',
+              'like', 'little', 'main', 'me', 'mine', 'more', 'my', 'now',
+              'of', 'off', 'oh', 'on', 'small', 'some', 'soon',
+              'that', 'the', 'then', 'this', 'those', 'through', 'till', 'to',
+              'towards', 'until', 'us', 'want', 'we', 'what', 'when', 'why',
+              'wish', 'with', 'would']
 
 def Parse_Input(user_input):
     output = ""
